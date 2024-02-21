2d97fa37418ec4441265394a1ce3c10a0771570d
Stas-Cell-Max <stanislav.morozan@gmail.com>
2024-02-20 22:25:17 -0500

    Fix merge conflicts in isAuthenticated.js

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -4,18 +4,7 @@
 // If the user is logged in, it calls next() to pass control to the next handler.
 
 function isAuthenticated(req, res, next) {
-<<<<<<< HEAD
-  if (req.session && req.session.userId) {
-      // The user is logged in, proceed to the next handler
-      next();
-  } else {
-      // The user is not logged in, send an unauthorized status
-      res.status(401).json({ message: "Unauthorized: Please log in." });
-  }
-}
 
-module.exports = isAuthenticated;
-=======
     if (req.session && req.session.userId) {
         // The user is logged in, proceed to the next handler
         next();
